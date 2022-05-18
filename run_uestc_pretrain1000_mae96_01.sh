8434f99bf57f306bd1faca49d78a13fc3dc168c9
XLiu443 <dlutliuxiao@gmail.com>
2022-05-18 03:33:20 +0200

    sh updated

--- run_uestc_pretrain1000_mae96_01.sh
+++ run_uestc_pretrain1000_mae96_01.sh
@@ -4,6 +4,9 @@
 #SBATCH --gres=gpu:a100:8
 #SBATCH --output=slurm_uestc_pretrain1000_mae_96_01.out
 
+module load anaconda
+conda activate myactor
+
 srun python -m torch.distributed.launch --nproc_per_node=8 --nnodes=1 --master_addr=127.0.3.1 --master_port=29531 -m src.train.train_cvae --modelname cvae_transformer_rc_rcxyz_kl_mae --pose_rep rot6d --lambda_kl 1e-5 --lambda_mae 0.1 --jointstype vertices --batch_size 160 --num_frames 60 --num_layers 8 --lr 0.0001 --glob --no-vertstrans --dataset uestc --num_epochs 500 --snapshot 100 --folder exps_uestc_pretrain1000_mae_96_01/uestc --resume2
 
 
