226b7a8b5909fe7b2b37a4dca8cfa4419d47d695
XLiu443 <dlutliuxiao@gmail.com>
2022-05-18 18:58:16 +0200

    zip

---
+++ zip_uestc_ckps.sh
@@ -0,0 +1,7 @@
+zip ckpts0.zip /fsx/sernamlim/xiaoliu/Compositional_Motion_UESTC/main/Compositional_Motion_UESTC/exps_actor_uestc/uestc/*.pth.tar
+
+zip ckpts1.zip /fsx/sernamlim/xiaoliu/Compositional_Motion_UESTC/uestc_alphauniform/Compositional_Motion_UESTC/exps_uestc_alphauniform/uestc/*.pth.tar
+
+zip ckpts2.zip /fsx/sernamlim/xiaoliu/Compositional_Motion_UESTC/uestc_nomask/Compositional_Motion_UESTC/exps_uestc_nomask/uestc/*.pth.tar
+
+zip ckpts3.zip /fsx/sernamlim/xiaoliu/Compositional_Motion_UESTC/uestc_alpha05/Compositional_Motion_UESTC/exps_uestc_alpha05/uestc/*.pth.tar
